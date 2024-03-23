f29a0628aa0c12a14e9cd1ced9996f3dba87af7b
Yevgeniy <theequimanthorn@gmail.com>
2024-03-23 14:19:48 +0000

    first commit

---
+++ app.ts
@@ -0,0 +1,48 @@
+import express, { Express, Request, Response } from 'express';
+import * as bodyParser from 'body-parser';
+import * as redis from 'redis';
+import { RedisClientType } from 'redis';
+import * as path from 'path';
+
+const HTTP_PORT = process.env.PORT || 3001;
+const REDIS_URL = process.env.REDIS_URL;
+
+const app: Express = express();
+app.use(bodyParser.json({ limit: '50mb' }));
+
+let publisher: RedisClientType;
+
+app.post('/', async (req: Request, res: Response) => {
+	const input = req.body.input || 'a';
+
+	const channel = 'message_queue';
+	const message = input;
+
+	try {
+		publisher.publish(channel, message);
+		console.log(`Sent: ${message}`);
+	} catch (e) {
+		console.error('PUB ERROR');
+		console.error(e, `\t`);
+	}
+
+	res.send('Express + TypeScript Server');
+});
+
+app.get('/', function (req, res) {
+	res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.listen(HTTP_PORT, () => {
+	console.log(`[server]: Server is running at http://localhost:${HTTP_PORT}`);
+});
+
+async function connect() {
+	publisher = redis.createClient({
+		url: REDIS_URL,
+	});
+
+	await publisher.connect();
+}
+
+connect();
